5f0d79619630ff8f407181a2d7f3eb6ab4b485f5
James Chapman <jchapman3000@gmail.com>
2017-03-12 08:38:11 +0000

    Initial state logic

--- Bull Kart/Assets/Scripts/PlayerController.cs
+++ Bull Kart/Assets/Scripts/PlayerController.cs
@@ -3,33 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+enum State {
+	PRE,
+	RACE,
+	FINISH
+}
+
 public class PlayerController : MonoBehaviour {
 
 	public Spline spline;
 	public KartController[] karts;
 
+	public int laps = 3;
+	public int countdown = 3;
+
+	private State state = State.PRE;
+	private KartController winner;
+
 	void Update() {
-		int position = 1;
+		switch (state) {
+		case State.PRE:
+			break;
+		case State.RACE:
+			int position = 1;
 
-		foreach (KartController kart in karts.OrderByDescending(kart => kart.Lap).ThenByDescending(kart => kart.SplinePosition)) {
-			string positionSuffix;
+			foreach (KartController kart in karts.OrderByDescending(kart => kart.Lap).ThenByDescending(kart => kart.SplinePosition)) {
+				string positionSuffix;
 
-			switch (position++) {
-			case 1:
-				positionSuffix = "st";
-				break;
-			case 2:
-				positionSuffix = "nd";
-				break;
-			case 3:
-				positionSuffix = "rd";
-				break;
-			default:
-				positionSuffix = "th";
-				break;
+				switch (position++) {
+				case 1:
+					positionSuffix = "st";
+					break;
+				case 2:
+					positionSuffix = "nd";
+					break;
+				case 3:
+					positionSuffix = "rd";
+					break;
+				default:
+					positionSuffix = "th";
+					break;
+				}
+
+				// TODO: Update kart position and lap in UI
+
+				if (kart.Lap >= laps && winner == null) {
+					winner = kart;
+					state = State.FINISH;
+				}
 			}
-
-			// TODO: Update kart position and lap in UI
+			break;
+		case State.FINISH:
+			break;
 		}
 	}
 }
