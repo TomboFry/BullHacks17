6d549caa12df169076e82e3ea48a1682c579f4c0
James Chapman <jchapman3000@gmail.com>
2017-03-12 05:18:36 +0000

    Add stub for UI update logic

--- Bull Kart/Assets/Scripts/PlayerController.cs
+++ Bull Kart/Assets/Scripts/PlayerController.cs
@@ -7,4 +7,29 @@ public class PlayerController : MonoBehaviour {
 
 	public Spline spline;
 	public KartController[] karts;
+
+	void Update() {
+		int position = 1;
+
+		foreach (KartController kart in karts.OrderBy(kart => kart.Lap).ThenByDescending(kart => kart.SplinePosition)) {
+			string positionSuffix;
+
+			switch (position++) {
+			case 1:
+				positionSuffix = "st";
+				break;
+			case 2:
+				positionSuffix = "nd";
+				break;
+			case 3:
+				positionSuffix = "rd";
+				break;
+			default:
+				positionSuffix = "th";
+				break;
+			}
+
+			// TODO: Update kart position and lap in UI
+		}
+	}
 }
